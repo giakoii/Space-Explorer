29f253dd54804fc53ffc59fc61cd623631db6701
hai300420 <haidoi3004@gmail.com>
2025-02-12 18:26:29 +0700

    Enemy

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] float health = 100;
+    [SerializeField] int scoreValue = 100;
+    [SerializeField] float speed = 5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Spawn();
+    }
+    private void Spawn()
+    {
+        Vector2 pos = transform.position;
+
+        pos = new Vector2(pos.x, pos.y - speed * Time.deltaTime);
+
+        transform.position = pos;
+
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+
+        if (transform.position.y < min.y)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
