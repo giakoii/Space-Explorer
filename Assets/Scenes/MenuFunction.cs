4920f4e340e10a5544b46d77b987cf455e4701fc
HoangPM <hoang@example.com>
2025-02-19 21:40:12 +0700

    Menu

---
+++ Assets/Scenes/MenuFunction.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuFunction : MonoBehaviour
+{
+    public void Play()
+    {
+
+    }
+    public void Exit()
+    {
+     
+    }
+
+}
